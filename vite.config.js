9178982faf68637e2cd203bc2eaff87ae4d1fd17
troyanvic.js <troyanvic@gmail.com>
2025-03-29 14:48:54 +0200

    chore: update cursor path and add new alias mappings
    - Added constants alias too

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@images": path.resolve(__dirname, "./src/assets/images"),
       "@fonts": path.resolve(__dirname, "./src/assets/fonts"),
+      "@cursors": path.resolve(__dirname, "./src/assets/cursors"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
       "@common": path.resolve(__dirname, "./src/common"),
       "@components": path.resolve(__dirname, "./src/components"),
       "@context": path.resolve(__dirname, "./src/context"),
