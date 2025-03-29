df1504ec6b7a9912f5ec9ff6cddbae40c12fb73a
troyanvic.js <troyanvic@gmail.com>
2025-03-29 16:30:07 +0200

    chore: add default props to `DialogButton` component

--- src/common/DialogButton/DialogButton.jsx
+++ src/common/DialogButton/DialogButton.jsx
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 // import styles
 import styles from "./DialogButton.module.scss";
 
+// import constants
+import { BUTTON_TYPE_OK, STATE_ACTIVE } from "@constants";
+
 /**
  * DialogButton is a functional component for rendering styled buttons used in dialog interactions.
  *
@@ -14,7 +17,7 @@ import styles from "./DialogButton.module.scss";
  *
  * The button's appearance is dynamically set based on the `type` and `state` props using CSS class names.
  */
-const DialogButton = memo(function Button({ type, state, onClick }) {
+const DialogButton = memo(function Button({ type = BUTTON_TYPE_OK, state = STATE_ACTIVE, onClick }) {
   const buttonClassName = `${styles.dialogBtn} ${styles[`dialog-btn--${type}--${state}`]}`;
 
   return <button className={buttonClassName} type="button" onClick={onClick} />;
