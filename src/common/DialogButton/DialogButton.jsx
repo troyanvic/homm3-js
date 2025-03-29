929d52a83b69f2b8c3dc03101ca7483e8836e522
troyanvic.js <troyanvic@gmail.com>
2025-03-29 18:03:49 +0200

    chore: add disabled state handling to `DialogButton` component

--- src/common/DialogButton/DialogButton.jsx
+++ src/common/DialogButton/DialogButton.jsx
@@ -4,7 +4,7 @@ import { memo } from "react";
 import styles from "./DialogButton.module.scss";
 
 // import constants
-import { BUTTON_TYPE_OK, STATE_ACTIVE } from "@constants";
+import { BUTTON_TYPE_OK, STATE_ACTIVE, STATE_DISABLED } from "@constants";
 
 /**
  * DialogButton is a functional component for rendering styled buttons used in dialog interactions.
@@ -17,9 +17,15 @@ import { BUTTON_TYPE_OK, STATE_ACTIVE } from "@constants";
  * The button's appearance is dynamically set based on the `type` and `state` props using CSS class names.
  */
 const DialogButton = memo(function Button({ type = BUTTON_TYPE_OK, state = STATE_ACTIVE, onClick }) {
+  // Add handler that checks if button is disabled
+  const handleClick = (e) => {
+    if (state === STATE_DISABLED) return;
+    onClick && onClick(e);
+  };
+
   const buttonClassName = `${styles.dialogBtn} ${styles[`dialog-btn--${type}--${state}`]}`;
 
-  return <button className={buttonClassName} type="button" onClick={onClick} />;
+  return <button className={buttonClassName} type="button" onClick={handleClick} />;
 });
 
 export default DialogButton;
