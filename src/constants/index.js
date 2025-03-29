984e07bc209979316d08ee3769bf49fdc0daaf38
troyanvic.js <troyanvic@gmail.com>
2025-03-29 20:25:37 +0200

    chore: add `constants` file with predefined values

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+// States
+export const STATE_ACTIVE = "active";
+export const STATE_DISABLED = "disabled";
+
+// Dialogs
+export const DIALOG_TYPE_MESSAGE = "message";
+
+// Buttons
+export const BUTTON_TYPE_OK = "ok";
+export const BUTTON_TYPE_CANCEL = "cancel";
