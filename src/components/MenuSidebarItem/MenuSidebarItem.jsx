925d3ebefa9ecf6fd3c5fbf15a7548d35b0e498f
troyanvic.js <troyanvic@gmail.com>
2025-04-04 20:05:20 +0300

    feat: add sound effect and delayed click to `MenuSidebarItem` component

--- src/components/MenuSidebarItem/MenuSidebarItem.jsx
+++ src/components/MenuSidebarItem/MenuSidebarItem.jsx
@@ -3,10 +3,32 @@ import { memo } from "react";
 // import styles
 import styles from "./MenuSidebarItem.module.scss";
 
-const MenuSidebarItem = memo(function MenuSidebarItem({ type }) {
+// import sound effects
+import buttonClickSound from "@sounds/button-click.ogg";
+
+// import constants
+import { STATE_ACTIVE, STATE_DISABLED } from "@constants";
+
+// import hooks
+import { useSoundEffect } from "@hooks/useSoundEffect";
+import { useDelayedClick } from "@hooks/useDelayedClick";
+
+const MenuSidebarItem = memo(function MenuSidebarItem({ type, state = STATE_ACTIVE, onClick }) {
+  const { playSound } = useSoundEffect(buttonClickSound);
+
+  // Use the custom hook to handle the delayed click
+  const handleDelayedClick = useDelayedClick(onClick, 75, state);
+
+  // Play sound on mouse down if the button is not disabled
+  const handleMouseDown = () => {
+    if (state === STATE_DISABLED) return;
+
+    playSound();
+  };
+
   const className = `${styles.menuItem} ${styles[`menu-item--${type}`]}`;
 
-  return <div className={className} />;
+  return <div className={className} onMouseDown={handleMouseDown} onClick={handleDelayedClick} />;
 });
 
 export default MenuSidebarItem;
