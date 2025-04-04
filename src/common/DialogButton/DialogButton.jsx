d4e87423b09646169b5af9a4e56d6e4a12e5fa4d
troyanvic.js <troyanvic@gmail.com>
2025-04-04 20:09:33 +0300

    refactor: replace custom logic with reusable hooks

--- src/common/DialogButton/DialogButton.jsx
+++ src/common/DialogButton/DialogButton.jsx
@@ -1,5 +1,4 @@
-import { memo, useRef, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { memo, useEffect } from "react";
 
 // import styles
 import styles from "./DialogButton.module.scss";
@@ -10,8 +9,9 @@ import buttonClickSound from "@sounds/button-click.ogg";
 // import constants
 import { BUTTON_TYPE_OK, STATE_ACTIVE, STATE_DISABLED } from "@constants";
 
-// import selectors
-import { selectEffectsVolume } from "@slices/systemOptionsSlice.js";
+// import hooks
+import { useSoundEffect } from "@hooks/useSoundEffect";
+import { useDelayedClick } from "@hooks/useDelayedClick";
 
 /**
  * DialogButton is a functional component for rendering styled buttons used in dialog interactions.
@@ -25,14 +25,9 @@ import { selectEffectsVolume } from "@slices/systemOptionsSlice.js";
  */
 // TODO: Resolve a problem with calling an audio effect as many times as the component mounted
 const DialogButton = memo(function Button({ type = BUTTON_TYPE_OK, state = STATE_ACTIVE, onClick }) {
-  // get Redux state
-  const effectsVolume = useSelector(selectEffectsVolume);
+  const { playSound } = useSoundEffect(buttonClickSound);
 
-  // Create references
-  const audioRef = useRef(new Audio(buttonClickSound));
-  const timeoutRef = useRef(null);
-
-  useEffect(() => {
+  /*useEffect(() => {
     const audio = audioRef.current;
 
     if (!audio) return;
@@ -45,39 +40,25 @@ const DialogButton = memo(function Button({ type = BUTTON_TYPE_OK, state = STATE
 
     // Pause the audio on component unmount
     return () => audio.pause();
-  }, []);
+  }, []);*/
+
+  // Use the custom hook to handle the delayed click
+  const handleDelayedClick = useDelayedClick(onClick, 75, state);
 
   // Play sound on mouse down if the button is not disabled
   const handleMouseDown = () => {
     if (state === STATE_DISABLED) return;
 
-    const audio = audioRef.current;
-
-    if (!audio) return;
-
-    // Reset the audio and play it
-    audio.currentTime = 0;
-    audio.play();
-  };
-
-  // Handle click event, ensuring the button is not disabled
-  const handleClick = (e) => {
-    if (state === STATE_DISABLED) return;
-
-    // Clear the timer if it exists
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-
-    // Execute the onClick callback if provided
-    timeoutRef.current = setTimeout(() => onClick && onClick(e), 75);
+    playSound();
   };
 
   // Construct the button's CSS class based on type and state props
   const buttonClassName = `${styles.dialogBtn} ${styles[`dialog-btn--${type}--${state}`]}`;
 
   // Render the button element
-  return <button className={buttonClassName} type="button" onMouseDown={handleMouseDown} onClick={handleClick} />;
+  return (
+    <button className={buttonClassName} type="button" onMouseDown={handleMouseDown} onClick={handleDelayedClick} />
+  );
 });
 
 export default DialogButton;
