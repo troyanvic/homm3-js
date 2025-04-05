f6fbfc40e0c5cda0d79c9d4cb9cfc2c3e59c7d20
troyanvic.js <troyanvic@gmail.com>
2025-04-05 14:16:40 +0300

    refactor: simplify click handling in `MenuSidebarItem` component

--- src/components/MenuSidebarItem/MenuSidebarItem.jsx
+++ src/components/MenuSidebarItem/MenuSidebarItem.jsx
@@ -3,32 +3,29 @@ import { memo } from "react";
 // import styles
 import styles from "./MenuSidebarItem.module.scss";
 
-// import sound effects
-import buttonClickSound from "@sounds/button-click.ogg";
-
 // import constants
-import { STATE_ACTIVE, STATE_DISABLED } from "@constants";
+import { STATE_ACTIVE } from "@constants";
 
 // import hooks
-import { useSoundEffect } from "@hooks/useSoundEffect";
-import { useDelayedClick } from "@hooks/useDelayedClick";
+import { useClickWithSound } from "@hooks/useClickWithSound.js";
+
+/**
+ * `MenuSidebarItem` is a memoized functional component that represents a menu item in the sidebar.
+ *
+ * Props:
+ * - `type` (string): Determines the specific type of the menu item, which is used to apply corresponding styles.
+ * - `state` (string, optional): The current state of the menu item. Defaults to `STATE_ACTIVE`.
+ * - `onClick` (function): A callback function triggered when the menu item is clicked.
+ */
 
 const MenuSidebarItem = memo(function MenuSidebarItem({ type, state = STATE_ACTIVE, onClick }) {
-  const { playSound } = useSoundEffect(buttonClickSound);
+  const { handleMouseDown, handleClick } = useClickWithSound(onClick, 75, state);
 
-  // Use the custom hook to handle the delayed click
-  const handleDelayedClick = useDelayedClick(onClick, 75, state);
-
-  // Play sound on mouse down if the button is not disabled
-  const handleMouseDown = () => {
-    if (state === STATE_DISABLED) return;
-
-    playSound();
-  };
-
+  // Construct the button's CSS class name based on type and state props
   const className = `${styles.menuItem} ${styles[`menu-item--${type}`]}`;
 
-  return <div className={className} onMouseDown={handleMouseDown} onClick={handleDelayedClick} />;
+  // Render the div element
+  return <div className={className} onMouseDown={handleMouseDown} onClick={handleClick} />;
 });
 
 export default MenuSidebarItem;
