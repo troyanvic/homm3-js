1b8b307c43ca51e4f8b59cc3be6e435999347676
troyanvic.js <troyanvic@gmail.com>
2025-04-05 13:38:55 +0300

    refactor: combine click and sound handling logic

--- src/common/DialogButton/DialogButton.jsx
+++ src/common/DialogButton/DialogButton.jsx
@@ -1,17 +1,13 @@
-import { memo, useEffect } from "react";
+import { memo } from "react";
 
 // import styles
 import styles from "./DialogButton.module.scss";
-
-// import sound effects
-import buttonClickSound from "@sounds/button-click.ogg";
 
 // import constants
 import { BUTTON_TYPE_OK, STATE_ACTIVE, STATE_DISABLED } from "@constants";
 
 // import hooks
-import { useSoundEffect } from "@hooks/useSoundEffect";
-import { useDelayedClick } from "@hooks/useDelayedClick";
+import { useClickWithSound } from "@hooks/useClickWithSound.js";
 
 /**
  * DialogButton is a functional component for rendering styled buttons used in dialog interactions.
@@ -23,41 +19,22 @@ import { useDelayedClick } from "@hooks/useDelayedClick";
  *
  * The button's appearance is dynamically set based on the `type` and `state` props using CSS class names.
  */
-// TODO: Resolve a problem with calling an audio effect as many times as the component mounted
 const DialogButton = memo(function Button({ type = BUTTON_TYPE_OK, state = STATE_ACTIVE, onClick }) {
-  const { playSound } = useSoundEffect(buttonClickSound);
+  const { handleMouseDown, handleClick } = useClickWithSound(onClick, 75, state);
 
-  /*useEffect(() => {
-    const audio = audioRef.current;
-
-    if (!audio) return;
-
-    // Preload the audio for faster playback
-    audio.preload = "auto";
-
-    // Set the system audio volume
-    audio.volume = effectsVolume;
-
-    // Pause the audio on component unmount
-    return () => audio.pause();
-  }, []);*/
-
-  // Use the custom hook to handle the delayed click
-  const handleDelayedClick = useDelayedClick(onClick, 75, state);
-
-  // Play sound on mouse down if the button is not disabled
-  const handleMouseDown = () => {
-    if (state === STATE_DISABLED) return;
-
-    playSound();
-  };
-
-  // Construct the button's CSS class based on type and state props
-  const buttonClassName = `${styles.dialogBtn} ${styles[`dialog-btn--${type}--${state}`]}`;
+  // Construct the button's CSS class name based on type and state props
+  const className = `${styles.dialogBtn} ${styles[`dialog-btn--${type}--${state}`]}`;
 
   // Render the button element
   return (
-    <button className={buttonClassName} type="button" onMouseDown={handleMouseDown} onClick={handleDelayedClick} />
+    <button
+      className={className}
+      type="button"
+      onMouseDown={handleMouseDown}
+      onClick={handleClick}
+      aria-label={`${type} button`}
+      disabled={state === STATE_DISABLED}
+    />
   );
 });
 
