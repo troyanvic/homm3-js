5593fb54c876b952c2d73546a7dc09556fbf1f75
troyanvic.js <troyanvic@gmail.com>
2025-04-26 22:29:42 +0300

    feat: add support for the New Game Campaign menu title

--- src/components/MenuTitle/MenuTitle.jsx
+++ src/components/MenuTitle/MenuTitle.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import style from "./MenuTitle.module.scss";
 
 // import constants
-import { MENU_TYPE_LOAD_GAME, MENU_TYPE_MAIN, MENU_TYPE_NEW_GAME } from "@constants";
+import { MENU_TYPE_LOAD_GAME, MENU_TYPE_MAIN, MENU_TYPE_NEW_GAME, MENU_TYPE_NEW_GAME_CAMPAIGN } from "@constants";
 
 // import selectors
 import { selectLanguage } from "@slices/systemOptionsSlice.js";
@@ -16,7 +16,7 @@ export default function MenuTitle({ type = MENU_TYPE_MAIN }) {
   // Construct the text class name based on the language
   const textClassName = `${style.menuTitleText} ${style[`menu-title-text--${type}-title-${language}`]}`;
 
-  if (type === MENU_TYPE_NEW_GAME || type === MENU_TYPE_LOAD_GAME) {
+  if (type === MENU_TYPE_NEW_GAME || type === MENU_TYPE_LOAD_GAME || type === MENU_TYPE_NEW_GAME_CAMPAIGN) {
     return (
       <div className={style.menuTitleContainer}>
         <div className={textClassName} />
